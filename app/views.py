a103e9720c5838ab295d5af6bfbf4b7089dcd777
Banita-12 <banitasamantara023@gmail.com>
2024-02-13 23:16:42 +0530

    create_update_delete

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from app.models import *
+from django.urls import reverse_lazy
 
 class SchoolList(ListView):
     model=School
@@ -13,3 +14,17 @@ class SchoolList(ListView):
 class SchoolDetail(DetailView):
     model=School
     context_object_name='sclobject'
+
+class SchoolCreate(CreateView):
+    model=School
+    fields='__all__'
+
+
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+
+class SchoolDelete(DeleteView):
+    model= School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
